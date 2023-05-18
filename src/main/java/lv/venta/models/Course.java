a8681b835cc6c34007adcb828e400e99e59294f0
localuser <localuser@VEA.LV>
2023-05-18 15:05:48 +0300

    ToString Exclude

--- src/main/java/lv/venta/models/Course.java
+++ src/main/java/lv/venta/models/Course.java
@@ -53,6 +53,7 @@ public class Course {
 	private Professor professor;
 	
 	@OneToMany(mappedBy = "course")
+	@ToString.Exclude
 	private Collection<Grade> grades;
 
 	public Course(
--- src/main/java/lv/venta/models/Student.java
+++ src/main/java/lv/venta/models/Student.java
@@ -47,6 +47,7 @@ public class Student {
 	private String surname;
 
 	@OneToMany(mappedBy = "student")
+	@ToString.Exclude
 	private Collection<Grade> grades;
 	
 	public Student(
